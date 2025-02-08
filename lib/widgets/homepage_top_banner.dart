fced300a816682e26aadee1a7e1a72205397ee68
JaehwanH <54016683+ja7811@users.noreply.github.com>
2025-02-09 01:10:56 +0900

    add: 홈페이지 상단 배너 구현 (#5)
    * add: AppColors 추가
    
    * del: gitkeep 삭제
    
    * add: api_service 생성
    
    * add: models 생성
    
    * add: 위젯 제작 중
    
    * add: NotoSansKR 글씨체 적용
    
    * feat: 홈페이지 상단 배너 구현

---
+++ lib/widgets/homepage_top_banner.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/core/app_colors.dart';
+
+
+
+class HomepageTopBanner extends StatelessWidget {
+  final String username;
+  const HomepageTopBanner({
+    super.key,
+    required this.username,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        color: Colors.white,
+        height: 192,
+        child: Row(
+          children: [
+            Expanded(
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontFamily: 'NotoSansKR',
+                  ),
+                  children: [
+                    TextSpan(text: "$username님!\n"),
+                    const TextSpan(text: "중요한 "),
+                    const TextSpan(
+                      text: "공지",
+                      style: TextStyle(color: AppColors.textPurple),
+                    ),
+                    const TextSpan(text: "를\n"),
+                    const TextSpan(
+                      text: "확인",
+                      style: TextStyle(color: AppColors.textPurple),
+                    ),
+                    const TextSpan(text: "해보세요 :)"),
+                  ],
+                ),
+              ),
+            ),
+            // 이미지
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset("assets/images/calendar.png", fit: BoxFit.contain),
+            )
+          ],
+        ),
+      );
+  }
+}
+
+
