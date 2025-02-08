3e348a3052aebd45b5f7c102cf461954951bb2ab
Minkyeong Kim <80468377+alsrudrl1220@users.noreply.github.com>
2025-02-09 06:37:35 +0900

    add: 다이얼로그 추가

--- lib/screens/notice_details.dart
+++ lib/screens/notice_details.dart
@@ -1,12 +1,8 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/core/app_colors.dart';
 import 'package:frontend/widgets/dropdown.dart';
+import 'package:frontend/widgets/tag_button.dart';
 import 'package:frontend/widgets/notice_card.dart';
-
-import '../widgets/category_card.dart';
-import '../widgets/homepage_top_banner.dart';
-import '../widgets/relevant_notice_card.dart';
-import '../widgets/tag_button.dart';
 
 class NoticeDetailsPage extends StatefulWidget {
   const NoticeDetailsPage({super.key});
@@ -42,6 +38,30 @@ class _NoticeDetailsPageState extends State<NoticeDetailsPage> {
     });
   }
 
+  // 다이얼로그 함수
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text('2025학년도 1학기 국가근로장학금 희망근로지 신청 안내', style: TextStyle(fontWeight: FontWeight.bold),),
+          titlePadding: EdgeInsets.all(30),  // 제목에 패딩 추가
+          content: Text('''1. 신청 기간 : 2025. 02. 07(금) ~ 2025. 02. 12(수)
+
+2. 신청 대상 : 2025학년도 1학기 1차 국가근로장학금을 신청한 학생 
+
+3. 신청 시 주의사항
+ 가. 학적 기준 소속 캠퍼스로 신청해야 합니다. 
+   학기 중 교내근로, 교외근로 선발
+          '''),
+          contentPadding: EdgeInsets.only(right: 30, left: 30, bottom: 30),  // 내용에 패딩 추가
+
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -60,6 +80,7 @@ class _NoticeDetailsPageState extends State<NoticeDetailsPage> {
                   // 1. 카테고리 선택
                   Text("카테고리", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
                   SizedBox(height: 10,),
+
                   DropdownComponent(
                     items: _categories,
                     selectedValue: _selectedCategory,
@@ -75,6 +96,7 @@ class _NoticeDetailsPageState extends State<NoticeDetailsPage> {
                   // 2. 여러 태그 버튼들
                   Text("태그", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
                   SizedBox(height: 10,),
+
                   SingleChildScrollView(
                     scrollDirection: Axis.horizontal, // 가로로 스크롤 가능
                     child: Row(
@@ -113,7 +135,7 @@ class _NoticeDetailsPageState extends State<NoticeDetailsPage> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text('# 나에게 맞는 공지 안내', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w700),),
-                    SizedBox(height: 5,),
+                    SizedBox(height: 5, ),
                   ],
                 ),
               ],
@@ -131,17 +153,20 @@ class _NoticeDetailsPageState extends State<NoticeDetailsPage> {
                     return Container(
                       margin: EdgeInsets.only(right: 15),  // 각 카드 간의 간격 조정
                       child: Center(
-                        child: Column(
-                          children: [
-                            NoticeCard(
-                                category: "장학금",
-                                title: "2025학년도 1학기 국가근로장학금 희망근로지 신청 안내",
-                                date: "2025.02.07",
-                                relevance: 73
-                            ),
-                            SizedBox(height: 8,),
-                          ],
-                        )
+                          child: Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () => _showDialog(context), // 여기서 함수를 래핑해서 호출
+                                child: NoticeCard(
+                                    category: "장학금",
+                                    title: "2025학년도 1학기 국가근로장학금 희망근로지 신청 안내",
+                                    date: "2025.02.07",
+                                    relevance: 73
+                                ),
+                              ),
+                              SizedBox(height: 8,),
+                            ],
+                          )
                       ),
                     );
                   }),
